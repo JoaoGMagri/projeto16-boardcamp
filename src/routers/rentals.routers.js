4ca035406f05491502d76e1ba1051b9d49bcc692
JoaoGMagri <jgmagri@hotmail.com>
2022-12-10 23:52:28 -0300

    Projeto finalizado

--- src/routers/rentals.routers.js
+++ src/routers/rentals.routers.js
@@ -3,11 +3,12 @@ import { Router } from "express";
 import { getRentals, postRentalsOpen, postRentalsClose, deleteRentals } from "../controllers/rentals.controllers.js"
 import { postRentalsOpenMD } from "../middleware/postRentalsOpen.middleware.js";
 import { postRentalsCloseMD } from "../middleware/postRentalsClose.middleware.js";
+import { deleteRentalsMD } from "../middleware/deleteRentals.middleware.js";
 const router = Router();
 
 router.get("/rentals", getRentals);
 router.post("/rentals", postRentalsOpenMD, postRentalsOpen);
 router.post("/rentals/:id/return", postRentalsCloseMD, postRentalsClose);
-router.delete("/rentals/:id", deleteRentals);
+router.delete("/rentals/:id", deleteRentalsMD, deleteRentals);
 
 export default router;
